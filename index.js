e15d6f34edf90def5520277f71b4403ed983ac2c
Abderraouf Addaoud <66469993+RaoufAdaoud@users.noreply.github.com>
2024-01-03 18:43:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+
+/**Detecting Button Press */
+
+
+
+for(var i = 0; i< document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function (){
+
+        var buttonInnerHtml = this.innerHTML;
+        makeSound(buttonInnerHtml);
+
+        makeAnimation(buttonInnerHtml);
+
+    });
+}
+
+
+/**Detecting Keyboard Press */
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+
+    
+    makeAnimation(event.key);
+  
+
+});
+
+
+
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+                
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+                
+            break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+                    
+            break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+                
+            break;
+
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+                
+            break;
+
+        case "l":
+            var kick = new Audio("sounds/kick.mp3");
+            kick.play();
+                
+            break;
+
+
+    
+        default:
+            break;
+    }
+}
+
+
+function makeAnimation (currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
+
+
+/*
+var audioPlay = new Audio("sounds/crash.mp3");
+    audioPlay.play();
+    */
+   
